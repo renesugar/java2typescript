f1f952bb1ec18c053f9ebf351193bdaeee238907
Grégory NAIN <gregory.nain@gmail.com>
2014-11-04 11:00:56 +0100

    Removing l and ds

--- org.kevoree.modeling.java2typescript/src/main/java/com/siliconmint/ts/translator/LiteralTranslator.java
+++ org.kevoree.modeling.java2typescript/src/main/java/com/siliconmint/ts/translator/LiteralTranslator.java
@@ -6,9 +6,14 @@ import com.intellij.psi.PsiLiteralExpression;
 
 public class LiteralTranslator extends Translator<PsiLiteralExpression> {
 
-  @Override
-  public void translate(PsiElementVisitor visitor, PsiLiteralExpression element, TranslationContext ctx) {
-    ctx.append(element.getText().trim());
-  }
+    @Override
+    public void translate(PsiElementVisitor visitor, PsiLiteralExpression element, TranslationContext ctx) {
+        String trimmed = element.getText().trim();
+        if( !trimmed.toLowerCase().equals("null") && (trimmed.toLowerCase().endsWith("l") || trimmed.toLowerCase().endsWith("d"))) {
+            ctx.append(trimmed.substring(0, trimmed.length()-1));
+        } else {
+            ctx.append(trimmed);
+        }
+    }
 
 }
