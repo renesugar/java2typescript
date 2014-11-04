1fd894bd788aea92096520c624337145a397f76f
Grégory NAIN <gregory.nain@gmail.com>
2014-11-04 16:37:41 +0100

    TypeCast generics fix

--- org.kevoree.modeling.java2typescript/src/main/java/com/siliconmint/ts/translator/TypeCastExpressionTranslator.java
+++ org.kevoree.modeling.java2typescript/src/main/java/com/siliconmint/ts/translator/TypeCastExpressionTranslator.java
@@ -6,10 +6,17 @@ import com.intellij.psi.PsiTypeCastExpression;
 
 public class TypeCastExpressionTranslator extends Translator<PsiTypeCastExpression> {
 
-  @Override
-  public void translate(PsiElementVisitor visitor, PsiTypeCastExpression element, TranslationContext ctx) {
-    ctx.append('<').append(TypeHelper.getType(element.getCastType().getType(), ctx)).append('>');
-    element.getOperand().accept(visitor);
-  }
+    @Override
+    public void translate(PsiElementVisitor visitor, PsiTypeCastExpression element, TranslationContext ctx) {
+        String type = TypeHelper.getType(element.getCastType().getType(), ctx);
+        String fqn = ctx.getClassImports().get(type);
+        if(fqn == null) {
+            fqn = ctx.getClassPackage() + "." + type;
+            ctx.append('<').append(type).append(TypeHelper.getGenericsIfAny(ctx, fqn)).append('>');
+        } else {
+            ctx.append('<').append(fqn).append(TypeHelper.getGenericsIfAny(ctx, fqn)).append('>');
+        }
+        element.getOperand().accept(visitor);
+    }
 
 }
