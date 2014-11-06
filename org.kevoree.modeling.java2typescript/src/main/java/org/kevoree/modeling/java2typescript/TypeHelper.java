2a5fa8e6157678bff041f607776df4e68e63a4f6
Grégory NAIN <gregory.nain@gmail.com>
2014-11-06 15:33:19 +0100

    callbackLike method typing

--- org.kevoree.modeling.java2typescript/src/main/java/org/kevoree/modeling/java2typescript/TypeHelper.java
+++ org.kevoree.modeling.java2typescript/src/main/java/org/kevoree/modeling/java2typescript/TypeHelper.java
@@ -40,14 +40,25 @@ public class TypeHelper {
         } else if(element instanceof PsiClassReferenceType) {
             PsiClass resolvedClass = ((PsiClassReferenceType) element).resolve();
             if(resolvedClass != null) {
-                String qualifiedName = resolvedClass.getQualifiedName();
-                if(qualifiedName != null) {
-                    result = resolvedClass.getQualifiedName();
-                }
-                if (resolvedClass.getTypeParameters().length > 0) {
-                    String[] generics = new String[resolvedClass.getTypeParameters().length];
-                    Arrays.fill(generics, "any");
-                    result += "<" + String.join(", ", generics) + ">";
+                if(isCallbackClass(resolvedClass)) {
+                    PsiMethod method = resolvedClass.getAllMethods()[0];
+                    PsiParameter[] parameters = method.getParameterList().getParameters();
+                    String[] methodParameters = new String[parameters.length];
+                    for(int i = 0; i < methodParameters.length; i++) {
+                        methodParameters[i] = parameters[i].getName() + " : " + printType(parameters[i].getType(), ctx);
+                    }
+                    result = "(" + String.join(", ", methodParameters) + ") => " + printType(method.getReturnType(), ctx);
+                    return result;
+                } else {
+                    String qualifiedName = resolvedClass.getQualifiedName();
+                    if (qualifiedName != null) {
+                        result = resolvedClass.getQualifiedName();
+                    }
+                    if (resolvedClass.getTypeParameters().length > 0) {
+                        String[] generics = new String[resolvedClass.getTypeParameters().length];
+                        Arrays.fill(generics, "any");
+                        result += "<" + String.join(", ", generics) + ">";
+                    }
                 }
             } else {
                 String tryJavaUtil = javaUtilsTypes.get(((PsiClassReferenceType) element).getClassName());
@@ -74,6 +85,10 @@ public class TypeHelper {
         return result;
     }
 
+
+    public static boolean isCallbackClass(PsiClass clazz) {
+        return clazz.isInterface() && clazz.getAllMethods().length==1;
+    }
 
 
     /* Remove above */
