4c52c0891772bc10fade4f3f1a949df5d7f37561
Grégory NAIN <gregory.nain@gmail.com>
2015-01-28 10:59:49 +0100

    Adding hashCode function on Strings

--- org.kevoree.modeling.java2typescript/src/main/resources/java.ts
+++ org.kevoree.modeling.java2typescript/src/main/resources/java.ts
@@ -46,6 +46,7 @@ interface String {
     matches :  (regEx:String) => boolean;
     //getBytes : () => number[];
     isEmpty : () => boolean;
+    hashCode : () => number;
 }
 
 class StringUtils {
@@ -73,6 +74,17 @@ String.prototype.isEmpty = function () {
 
 String.prototype.equals = function (other) {
     return this == other;
+};
+
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr, len;
+    if (this.length == 0) return hash;
+    for (i = 0, len = this.length; i < len; i++) {
+        chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
 };
 
 String.prototype.startsWith = function (other) {
