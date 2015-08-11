f9789aae14dbe039476b8a3195e7067de019bd47
Maxime Tricoire <max.tricoire@gmail.com>
2015-08-11 15:52:23 +0200

    clean java.ts & junit.ts for global & umd

---
+++ org.kevoree.modeling.java2typescript.runtime/src/main/ts/umd/src/junit.d.ts
@@ -0,0 +1,11 @@
+declare module 'junit' {
+  export class Assert {
+      static assertArrayEquals(p: any, p2: any): void;
+      static assertNotNull(p: any): void;
+      static assertNull(p: any): void;
+      static assertEquals(p: any, p2: any): void;
+      static assertNotEquals(p: any, p2: any): void;
+      static assertTrue(b: boolean): void;
+      static assertFalse(b: boolean): void;
+  }
+}
