f408a91b82142bbbfd7a7721ff59946261df90db
Grégory Nain <gregory.nain@gmail.com>
2015-09-25 15:00:08 +0200

    test commit

--- org.kevoree.modeling.java2typescript.transpiler/src/test/java/TranspilerTest.java
+++ org.kevoree.modeling.java2typescript.transpiler/src/test/java/TranspilerTest.java
@@ -7,11 +7,14 @@ import java.io.IOException;
  */
 public class TranspilerTest {
 
-    private static final String baseDir = "/Users/gnain/Sources/Kevoree-Modeling/java2typescript/org.kevoree.modeling.java2typescript.transpiler/src/test/resources";
+    //private static final String baseDir = "/Users/gnain/Sources/Kevoree-Modeling/java2typescript/org.kevoree.modeling.java2typescript.transpiler/src/test/resources";
+    private static final String baseDir = "/Users/gnain/Sources/Kevoree-Modeling/framework/org.kevoree.modeling.microframework/src/main/java";
 
     public static void main(String[] args) throws IOException {
         SourceTranslator sourceTranslator = new SourceTranslator();
-        sourceTranslator.translateSources(baseDir, "target", "out", false, false, true);
+        sourceTranslator.getAnalyzer().addClasspath("/Users/gnain/.m2/repository/org/kevoree/modeling/org.kevoree.modeling.microframework/4.25.1-SNAPSHOT/org.kevoree.modeling.microframework-4.25.1-SNAPSHOT.jar");
+        sourceTranslator.getAnalyzer().addClasspath("/Users/gnain/.m2/repository/junit/junit/4.11/junit-4.11.jar");
+        sourceTranslator.translateSources(baseDir, "target", "out", false, false, false);
     }
 
 
