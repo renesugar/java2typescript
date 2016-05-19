07f06cfa90bc84ba6c77ba05df85f8816e8f54e7
Francois Fouquet <fouquet.f@gmail.com>
2016-05-19 18:20:14 +0200

    generate real exception for debug purpose

--- transpiler/src/main/resources/junit.ts
+++ transpiler/src/main/resources/junit.ts
@@ -6,35 +6,35 @@ module org {
                     if (p2 == null || p2 == undefined) {
                         return;
                     } else {
-                        throw "Assert Error " + p + " and " + p2 + " must be equals"
+                        throw new Error("Assert Error " + p + " and " + p2 + " must be equals");
                     }
                 }
                 if (p2 == null || p2 == undefined) {
                     if (p == null || p == undefined) {
                         return;
                     } else {
-                        throw "Assert Error " + p + " and " + p2 + " must be equals"
+                        throw new Error("Assert Error " + p + " and " + p2 + " must be equals");
                     }
                 }
                 if (p.length != p2.length) {
-                    throw "Assert Error " + p + " and " + p2 + " must be equals"
+                    throw new Error("Assert Error " + p + " and " + p2 + " must be equals");
                 }
                 for (var i = 0; i < p.length; i++) {
                     if (p[i] != p2[i]) {
-                        throw "Assert Error " + p + " and " + p2 + " must be equals"
+                        throw new Error("Assert Error " + p + " and " + p2 + " must be equals");
                     }
                 }
             }
 
             public static assertNotNull(p:any):void {
                 if (p == null) {
-                    throw "Assert Error " + p + " must not be null";
+                    throw new Error("Assert Error " + p + " must not be null");
                 }
             }
 
             public static assertNull(p:any):void {
                 if (p != null) {
-                    throw "Assert Error " + p + " must be null";
+                    throw new Error("Assert Error " + p + " must be null");
                 }
             }
 
@@ -43,23 +43,23 @@ module org {
                     if (p2 == null) {
                         return;
                     } else {
-                        throw "Assert Error: " + p3 + "\n" + p + "\n must be equal to \n" + p2 + "\n";
+                        throw new Error("Assert Error: " + p3 + "\n" + p + "\n must be equal to \n" + p2 + "\n");
                     }
                 }
                 if (p2 == null) {
                     if (p == null) {
                         return;
                     } else {
-                        throw "Assert Error: " + p3 + "\n" + p + "\n must be equal to \n" + p2 + "\n";
+                        throw new Error("Assert Error: " + p3 + "\n" + p + "\n must be equal to \n" + p2 + "\n");
                     }
                 }
                 if (p.equals !== undefined) {
                     if (!p.equals(p2)) {
-                        throw "Assert Error: " + p3 + "\n" + p + "\n must be equal to \n" + p2 + "\n";
+                        throw new Error("Assert Error: " + p3 + "\n" + p + "\n must be equal to \n" + p2 + "\n");
                     }
                 } else {
                     if (p != p2) {
-                        throw "Assert Error: " + p3 + "\n" + p + "\n must be equal to \n" + p2 + "\n";
+                        throw new Error("Assert Error: " + p3 + "\n" + p + "\n must be equal to \n" + p2 + "\n");
                     }
                 }
             }
@@ -67,29 +67,29 @@ module org {
             public static assertNotEquals(p:any, p2:any):void {
                 if (p.equals !== undefined) {
                     if (p.equals(p2)) {
-                        throw "Assert Error \n" + p + "\n must not be equal to \n" + p2 + "\n";
+                        throw new Error("Assert Error \n" + p + "\n must not be equal to \n" + p2 + "\n");
                     }
                 } else {
                     if (p == p2) {
-                        throw "Assert Error \n" + p + "\n must not be equal to \n" + p2 + "\n";
+                        throw new Error("Assert Error \n" + p + "\n must not be equal to \n" + p2 + "\n");
                     }
                 }
             }
 
             public static assertTrue(b:boolean):void {
                 if (!b) {
-                    throw "Assert Error " + b + " must be true";
+                    throw new Error("Assert Error " + b + " must be true");
                 }
             }
 
             public static assertFalse(b:boolean):void {
                 if (b) {
-                    throw "Assert Error " + b + " must be false";
+                    throw new Error("Assert Error " + b + " must be false");
                 }
             }
 
             public static fail(msg:string):void {
-                throw "Assert Error \n" + msg + "\n";
+                throw new Error("Assert Error \n" + msg + "\n");
             }
         }
     }
