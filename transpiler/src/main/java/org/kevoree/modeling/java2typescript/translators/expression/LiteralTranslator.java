427f0b41a2694fbef34b79f5249ee3420146cf8f
Grégory Nain <gregory.nain@gmail.com>
2016-05-25 10:22:26 +0200

    humpf

--- transpiler/src/main/java/org/kevoree/modeling/java2typescript/translators/expression/LiteralTranslator.java
+++ transpiler/src/main/java/org/kevoree/modeling/java2typescript/translators/expression/LiteralTranslator.java
@@ -8,16 +8,27 @@ public class LiteralTranslator {
 
     public static void translate(PsiLiteralExpression element, TranslationContext ctx) {
         String value = element.getText().trim();
-        //if (element.getType() != null && element.getType().getPresentableText().equals("char")) {
-         //   ctx.append(value);
-            //ctx.append(".charCodeAt(0)");
-        //} else {
-            if(!value.toLowerCase().equals("null") && (value.toLowerCase().endsWith("l") || value.toLowerCase().endsWith("d") || value.toLowerCase().endsWith("f"))) {
-                ctx.append(value.substring(0, value.length()-1));
-            } else {
+
+        if (value.toLowerCase().equals("null")) {
+            ctx.append(value);
+            return;
+        }
+
+        if (value.toLowerCase().endsWith("l")) {
+            ctx.append(value.substring(0, value.length() - 1));
+            return;
+        }
+
+        if (value.toLowerCase().endsWith("f") || value.toLowerCase().endsWith("d")) {
+            if (value.contains("0x")) {
                 ctx.append(value);
+            } else {
+                ctx.append(value.substring(0, value.length() - 1));
             }
-        //}
+            return;
+        }
+        ctx.append(value);
+
     }
 
 }
