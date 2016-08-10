ff4fa87acb57351058af92c57ab5bba247f80abe
Grégory Nain <gregory.nain@gmail.com>
2016-08-10 11:56:35 +0200

    first tests for #J2TS-3

---
+++ transpiler/src/test/java/sources/closures/ClosureInterfaceWithParam.java
@@ -0,0 +1,10 @@
+package sources.closures;
+
+/**
+ * Created by gnain on 10/08/16.
+ */
+public interface ClosureInterfaceWithParam {
+
+    void methodParam(boolean b);
+        
+}
---
+++ transpiler/src/test/java/sources/closures/ClosureInterfaceWithParamAndResult.java
@@ -0,0 +1,10 @@
+package sources.closures;
+
+/**
+ * Created by gnain on 10/08/16.
+ */
+public interface ClosureInterfaceWithParamAndResult {
+
+    boolean methodParam(boolean b);
+        
+}
