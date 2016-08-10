8717f90d8425f50a585f588179963d5c70e7f7e5
Grégory Nain <gregory.nain@gmail.com>
2016-08-10 13:10:09 +0200

    name fix (#J2TS-3)

--- transpiler/src/main/java/org/kevoree/modeling/java2typescript/translators/expression/ExpressionTranslator.java
+++ transpiler/src/main/java/org/kevoree/modeling/java2typescript/translators/expression/ExpressionTranslator.java
@@ -46,7 +46,7 @@ public class ExpressionTranslator {
         } else if (expression instanceof PsiClassObjectAccessExpression) {
             ClassObjectAccessExpressionTranslator.translate((PsiClassObjectAccessExpression) expression, ctx);
         } else if (expression instanceof PsiLambdaExpression) {
-            LambaExpressionTranslator.translate((PsiLambdaExpression) expression, ctx);
+            LambdaExpressionTranslator.translate((PsiLambdaExpression) expression, ctx);
         } else {
             System.err.println("EXPR " + expression);
         }
--- transpiler/src/main/java/org/kevoree/modeling/java2typescript/translators/expression/LambdaExpressionTranslator.java
+++ transpiler/src/main/java/org/kevoree/modeling/java2typescript/translators/expression/LambdaExpressionTranslator.java
@@ -1,19 +1,17 @@
 
 package org.kevoree.modeling.java2typescript.translators.expression;
 
-import com.intellij.psi.PsiCodeBlock;
-import com.intellij.psi.PsiLambdaExpression;
-import com.intellij.psi.PsiParameter;
+import com.intellij.psi.*;
 import org.kevoree.modeling.java2typescript.context.TranslationContext;
 import org.kevoree.modeling.java2typescript.helper.KeywordHelper;
 import org.kevoree.modeling.java2typescript.translators.CodeBlockTranslator;
 
-public class LambaExpressionTranslator {
+public class LambdaExpressionTranslator {
 
     public static void translate(PsiLambdaExpression element, TranslationContext ctx) {
         ctx.append("(");
         PsiParameter[] paramList = element.getParameterList().getParameters();
-        for (int i=0; i < paramList.length; i++) {
+        for (int i = 0; i < paramList.length; i++) {
             PsiParameter param = paramList[i];
             ctx.append(KeywordHelper.process(param.getName(), ctx));
             if (i < paramList.length - 1) {
@@ -22,7 +20,14 @@ public class LambaExpressionTranslator {
         }
         ctx.append(") => {\n");
         ctx.increaseIdent();
-        CodeBlockTranslator.translate((PsiCodeBlock) element.getBody(), ctx);
+        PsiElement body = element.getBody();
+        if (body instanceof PsiCodeBlock) {
+            CodeBlockTranslator.translate((PsiCodeBlock) body, ctx);
+        } else if (body instanceof PsiMethodCallExpression) {
+            MethodCallExpressionTranslator.translate((PsiMethodCallExpression) body, ctx);
+        } else {
+            System.err.println("LambdaExpressionTranslator:: Unsupported body type:" + body.getClass().getName());
+        }
         ctx.decreaseIdent();
         ctx.print("}");
     }
