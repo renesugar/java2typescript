7bfa448a5e0e6374bf19c453486c4009e517e7b9
Grégory Nain <gregory.nain@gmail.com>
2016-12-06 14:32:09 +0100

    clean

--- transpiler/src/test/java/org/kevoree/modeling/java2typescript/SourceTranslatorTest.java
+++ transpiler/src/test/java/org/kevoree/modeling/java2typescript/SourceTranslatorTest.java
@@ -138,7 +138,7 @@ public class SourceTranslatorTest {
         translator.process();
 
         String result = translator.getCtx().toString().trim();
-        System.out.println(result);
+        //System.out.println(result);
 
         BufferedReader br = new BufferedReader(new FileReader(Paths.get("src", "test", "resources", "varargs", "output.ts").toFile()));
         StringBuilder sb = new StringBuilder();
@@ -152,7 +152,7 @@ public class SourceTranslatorTest {
                 result);
     }
 
-    @Test
+    //@Test
     public void mwdb_core() throws IOException {
         String source = "/Users/gnain/Sources/Kevoree-Modeling/mwDB/core/src/main/java";
         //String source = "/Users/gnain/Sources/Kevoree-Modeling/mwDB/api/src/main/java";
